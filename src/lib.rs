469b32b69de3bd25317d5a880ca2eadcca3909df
octopirate <octo@pirate.rip>
2022-05-17 16:44:52 -1000

    Move all generator values inside Map struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,26 @@ use ansi_term::Colour::RGB;
 use image::{ImageBuffer, Rgb};
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
-use std::boxed::Box;
 use std::fmt;
 use std::path::Path;
 
 const WIGGLE: i16 = 32;
 const TILES: usize = 512;
-const SIZE: usize = TILES * 2 + 1;
 
 pub fn generate() {
-    let mut img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(SIZE as u32, SIZE as u32);
+    let mut map: Map = Map::new(WIGGLE, TILES);
+    
+    let mut img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(map.size as u32, map.size as u32);
 
-    let mut map: Map = Map::new();
-
-    let steps = TILES.trailing_zeros() + 1;
+    let steps = map.tiles.trailing_zeros() + 1;
 
     for s in 0..steps {
         map.squares(1 << s);
         map.diamonds(1 << (steps - s - 1));
     }
 
-    for x in 0..SIZE {
-        for y in 0..SIZE {
+    for x in 0..map.size {
+        for y in 0..map.size {
             img_buffer.put_pixel(x as u32, y as u32, Rgb([map.terrain[x][y] as u8, 13, 50]));
         }
     }
@@ -32,21 +30,29 @@ pub fn generate() {
 }
 
 struct Map {
-    pub terrain: Box<[[u32; SIZE as usize]; SIZE as usize]>,
+    pub terrain: Vec<Vec<u32>>,
+    pub wiggle_range: i16,
+    pub tiles: usize,
+    pub size: usize
 }
 
 impl Map {
-    fn new() -> Self {
-        let mut terrain = Box::new([[0; SIZE as usize]; SIZE as usize]);
+    fn new(wiggle_range: i16, tiles: usize) -> Self {
+        let size: usize = tiles * 2 + 1;
+
+        let mut terrain = vec![vec![0; size]; size];
 
         let mut rng = SmallRng::from_entropy();
         terrain[0][0]               = rng.gen::<u8>() as u32;
-        terrain[SIZE - 1][0]        = rng.gen::<u8>() as u32;
-        terrain[0][SIZE - 1]        = rng.gen::<u8>() as u32;
-        terrain[SIZE - 1][SIZE - 1] = rng.gen::<u8>() as u32;
+        terrain[size - 1][0]        = rng.gen::<u8>() as u32;
+        terrain[0][size - 1]        = rng.gen::<u8>() as u32;
+        terrain[size - 1][size - 1] = rng.gen::<u8>() as u32;
 
         Map {
             terrain,
+            wiggle_range,
+            tiles,
+            size
         }
     }
 
@@ -57,8 +63,7 @@ impl Map {
                 self.terrain[x + radius][y - radius] +
                 self.terrain[x + radius][y + radius] )
                 / 4,
-            WIGGLE,
-        );
+            );
 
         self.terrain[x][y] = height;
     }
@@ -72,7 +77,7 @@ impl Map {
             t += self.terrain[x - radius][y];
         }
 
-        if x + radius < SIZE {
+        if x + radius < self.size {
             spread += 1;
             t += self.terrain[x + radius][y];
         }
@@ -82,12 +87,12 @@ impl Map {
             t += self.terrain[x][y - radius];
         }
 
-        if y + radius < SIZE {
+        if y + radius < self.size {
             spread += 1;
             t += self.terrain[x][y + radius];
         }
 
-        let height = self.wiggle(t / spread, WIGGLE);
+        let height = self.wiggle(t / spread);
         self.terrain[x][y] = height;
     }
 
@@ -95,37 +100,37 @@ impl Map {
         for x in 0..step {
             for y in 0..step {
                 self.square(
-                    SIZE / (step * 2) + (x * SIZE / step),
-                    SIZE / (step * 2) + (y * SIZE / step),
-                    SIZE / (step * 2),
+                    self.size / (step * 2) + (x * self.size / step),
+                    self.size / (step * 2) + (y * self.size / step),
+                    self.size / (step * 2),
                 );
             }
         }
     }
 
     fn diamonds(&mut self, radius: usize) {
-        for x in (0..SIZE).step_by(radius) {
+        for x in (0..self.size).step_by(radius) {
             let start: usize = if (x / (radius)) % 2 == 0 { radius } else { 0 };
 
-            for y in (start..SIZE).step_by(radius * 2) {
+            for y in (start..self.size).step_by(radius * 2) {
                 self.diamond(x, y, radius);
             }
         }
     }
 
-    fn wiggle(&mut self, value: u32, range: i16) -> u32 {
-        let min = range.min(value as i16);
+    fn wiggle(&mut self, value: u32) -> u32 {
+        let min = self.wiggle_range.min(value as i16);
 
         let mut rng = SmallRng::from_entropy();
 
-        (value as i16 + rng.gen_range(-min, range)) as u32
+        (value as i16 + rng.gen_range(-min, self.wiggle_range)) as u32
     }
 }
 
 impl fmt::Display for Map {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for x in 0..SIZE {
-            for y in 0..SIZE {
+        for x in 0..self.size {
+            for y in 0..self.size {
                 let color = self.terrain[x][y] as u8;
                 let styled = RGB(color, 70, 130).paint("███");
                 let _ = write!(f, "{}", styled);
