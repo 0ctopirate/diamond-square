5f80be1e57c9eab71794b5a870f35f2bb9eb619a
octopirate <octo@pirate.rip>
2022-05-17 14:11:36 -1000

    Change terrain to be a multi-dimensional array

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,11 @@ use std::fmt;
 use std::path::Path;
 
 const WIGGLE: i16 = 32;
-const TILES: u32 = 512;
-const SIZE: u32 = TILES * 2 + 1;
-const MSIZE: usize = (SIZE * SIZE) as usize;
+const TILES: usize = 512;
+const SIZE: usize = TILES * 2 + 1;
 
 pub fn generate() {
-    let mut img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(SIZE, SIZE);
+    let mut img_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(SIZE as u32, SIZE as u32);
 
     let mut map: Map = Map::new();
 
@@ -27,7 +26,7 @@ pub fn generate() {
 
     for x in 0..SIZE {
         for y in 0..SIZE {
-            img_buffer.put_pixel(x, y, Rgb([map.get(x, y) as u8, 13, 50]));
+            img_buffer.put_pixel(x as u32, y as u32, Rgb([map.terrain[x][y] as u8, 13, 50]));
         }
     }
 
@@ -35,24 +34,16 @@ pub fn generate() {
 }
 
 struct Map {
+    pub terrain: Box<[[u32; SIZE as usize]; SIZE as usize]>,
     rng: SmallRng,
-    terrain: Box<[u32; MSIZE]>,
 }
 
 impl Map {
     fn new() -> Map {
         Map {
             rng: SmallRng::from_entropy(),
-            terrain: Box::new([0; MSIZE]),
+            terrain: Box::new([[0; SIZE as usize]; SIZE as usize]),
         }
-    }
-
-    fn get(&self, x: u32, y: u32) -> u32 {
-        self.terrain[(x * SIZE + y) as usize]
-    }
-
-    fn set(&mut self, x: u32, y: u32, v: u32) {
-        self.terrain[(x * SIZE + y) as usize] = v;
     }
 
     fn init(&mut self) {
@@ -61,82 +52,77 @@ impl Map {
         let c = self.rng.gen::<u8>() as u32;
         let d = self.rng.gen::<u8>() as u32;
 
-        self.set(0, 0, a);
-        self.set(SIZE - 1, 0, b);
-        self.set(0, SIZE - 1, c);
-        self.set(SIZE - 1, SIZE - 1, d);
+        self.terrain[0][0] = a;
+        self.terrain[SIZE - 1][0] = b;
+        self.terrain[0][SIZE - 1] = c;
+        self.terrain[SIZE - 1][SIZE - 1] = d;
     }
 
-    fn square(&mut self, x: u32, y: u32, radius: u32) {
-        let height = self.wiggle(
-            (self.get(x - radius, y - radius)
-                + self.get(x - radius, y + radius)
-                + self.get(x + radius, y - radius)
-                + self.get(x + radius, y + radius))
+    fn square(&mut self, x: usize, y: usize, radius: usize) {
+        let height = self.wiggle((
+                self.terrain[x - radius][y - radius] +
+                self.terrain[x - radius][y + radius] +
+                self.terrain[x + radius][y - radius] +
+                self.terrain[x + radius][y + radius] )
                 / 4,
             WIGGLE,
         );
 
-        self.set(x, y, height);
+        self.terrain[x][y] = height;
     }
 
-    fn diamond(&mut self, x: u32, y: u32, radius: u32) {
+    fn diamond(&mut self, x: usize, y: usize, radius: usize) {
         let mut spread = 0;
         let mut t = 0;
 
         if radius <= x {
             spread += 1;
-            t += self.get(x - radius, y);
+            t += self.terrain[x - radius][y];
         }
 
         if x + radius < SIZE {
             spread += 1;
-            t += self.get(x + radius, y);
+            t += self.terrain[x + radius][y];
         }
 
         if radius <= y {
             spread += 1;
-            t += self.get(x, y - radius);
+            t += self.terrain[x][y - radius];
         }
 
         if y + radius < SIZE {
             spread += 1;
-            t += self.get(x, y + radius);
+            t += self.terrain[x][y + radius];
         }
 
         let height = self.wiggle(t / spread, WIGGLE);
-        self.set(x, y, height);
+        self.terrain[x][y] = height;
     }
 
-    fn squares(&mut self, step: u32) {
-        let step2 = step * 2;
+    fn squares(&mut self, step: usize) {
         for x in 0..step {
             for y in 0..step {
                 self.square(
-                    SIZE / step2 + (x * SIZE / step),
-                    SIZE / step2 + (y * SIZE / step),
-                    SIZE / step2,
+                    SIZE / (step * 2) + (x * SIZE / step),
+                    SIZE / (step * 2) + (y * SIZE / step),
+                    SIZE / (step * 2),
                 );
             }
         }
     }
 
-    fn diamonds(&mut self, radius: u32) {
-        for x in (0..SIZE).step_by(radius as usize) {
-            let y_start: u32 = if (x / (radius)) % 2 == 0 { radius } else { 0 };
+    fn diamonds(&mut self, radius: usize) {
+        for x in (0..SIZE).step_by(radius) {
+            let start: usize = if (x / (radius)) % 2 == 0 { radius } else { 0 };
 
-            for y in (y_start..SIZE).step_by(radius as usize * 2) {
+            for y in (start..SIZE).step_by(radius * 2) {
                 self.diamond(x, y, radius);
             }
         }
     }
 
     fn wiggle(&mut self, value: u32, range: i16) -> u32 {
-        let min = if value < range as u32 {
-            value as i16
-        } else {
-            range
-        };
+        let min = range.min(value as i16);
 
         (value as i16 + self.rng.gen_range(-min, range)) as u32
     }
@@ -146,7 +132,7 @@ impl fmt::Display for Map {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for x in 0..SIZE {
             for y in 0..SIZE {
-                let color = self.get(x, y) as u8;
+                let color = self.terrain[x][y] as u8;
                 let styled = RGB(color, 70, 130).paint("███");
                 let _ = write!(f, "{}", styled);
             }
