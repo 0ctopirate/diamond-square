a3360641594f686c9231b171d3dc8fa23e60fa76
octopirate <octo@pirate.rip>
2022-05-18 15:26:08 -1000

    Add normalize method to terrain

--- src/terrain.rs
+++ src/terrain.rs
@@ -16,6 +16,21 @@ impl<T: Default + Clone> Terrain<T> {
 	}
 }
 
+impl<T: Default + Copy + Into<f64>> Terrain<T> {
+	pub fn normalize(self) -> Terrain<f64> {
+		let cast_map: Vec<f64> = self.map.iter().map(|&e| e.into()).collect();
+		let min = cast_map.iter().fold(f64::INFINITY, |a, &b| a.min(b));
+		let max = cast_map.iter().fold(f64::INFINITY, |a, &b| a.max(b));
+		let diff = max - min;
+
+		Terrain::<f64> {
+			map: cast_map.iter().map(|&e| (e - min) * diff).collect::<Vec<f64>>().into_boxed_slice(),
+			width: self.width,
+			height: self.height
+		}
+	}
+}
+
 impl<T: Default + Clone> Index<usize> for Terrain<T> {
 	type Output = [T];
 
